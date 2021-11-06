5a8ea191bede0bf86671381f6851aff11735f77a
alemenew <alemenewendawek@gmail.com>
2021-11-06 10:39:39 +0300

    first commit

---
+++ unittest.py
@@ -0,0 +1,12 @@
+x = input ('Enter a number: ')
+y = input ('Enter a number": ')
+def add(x+y):
+    return x , y
+def sun (x ,y):
+    return x - y
+def mul(x , y):
+    return x * y
+def div (x , y):
+    if y == 0:
+        raise ValueError ('can not divide by zero!')
+    return x / y
